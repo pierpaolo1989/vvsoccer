63732c98a6bddece0b4b0b031b95d7f2f45c37a4
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-08-30 12:12:00 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:soccer/models/ResponseVideo.dart';
-import 'package:soccer/models/VideoContent.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:soccer/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,71 +25,27 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
-
-  List<VideoContent> videos = [];
-  var jsonData = null;
-   final _htmlContent = """
-  <div style='width:100%;height:0px;position:relative;padding-bottom:56.250%;'><iframe src='https:\/\/www.scorebat.com\/embed\/v\/6128c4483364d\/?utm_source=api&utm_medium=video&utm_campaign=v3' frameborder='0' width='100%' height='100%' allowfullscreen allow='autoplay; fullscreen' style='width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;'><\/iframe><\/div>
-  """;
-
-  @override
+ 
+ @override
   void initState() {
-    getVideos();
+     Timer(Duration(seconds: 5),
+              ()=>Navigator.pushReplacement(context,
+              MaterialPageRoute(builder:
+                  (context) => Homepage(),
+              )
+          )
+      );
     super.initState();
   }
-
-  Future getVideos() async {
-    await http.get(Uri.parse("https://www.scorebat.com/video-api/v3/")).then((res) {
-      if(res.statusCode==200){
-        jsonData = json.decode(res.body);
-        ResponseVideo response = ResponseVideo.fromMap(jsonData);
-        setState(() {
-          videos = response.response;
-        });
-      }
-    });
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("VVSoccer"),
-      ),
-      body: Center(
-        child: Container(
-          child: ListView.builder(
-              itemCount: videos.length,
-              itemBuilder: (context, index) {
-                return Card( //                           <-- Card widget
-                child: Container(
-                  child:
-                     ListTile(
-                       title: Text(videos[index].title),
-                       subtitle: Text(videos[index].competition),
-                       onTap:(){
-
-                       },
-                       leading:ConstrainedBox(
-                         constraints: BoxConstraints(
-                           minWidth: 144,
-                           minHeight: 144,
-                           maxWidth: 164,
-                           maxHeight: 164,
-                         ),
-                         child: Html(
-                           data: _htmlContent,
-                         ),
-                     ),
-            ),
-        )
-                );
-      },
-    )
-        )
-      ),
+     return Container(
+        color: Colors.white,
+        child: Image(image: AssetImage('assets/logo.png'))
     );
-}
+  }
 
 }
